2638d634c06eacb3194847e638ccc39fd14efdd0
Sheema Syed <shma.syd975@gmail.com>
2025-09-10 15:37:37 +0530

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/home_screen.dart';
 import 'services/map_service.dart';
+import 'services/supabase_service.dart';
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +20,14 @@ void main() async {
 
 Future<void> _initializeServices() async {
   // TODO: Replace with your actual Supabase credentials
-  // await SupabaseService.instance.initialize(
-  //   url: 'YOUR_SUPABASE_URL',
-  //   anonKey: 'YOUR_SUPABASE_ANON_KEY',
-  // );
+
+  await SupabaseService.instance.initialize(
+    url: 'https://xuyzomlepudifwfbxmrl.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1eXpvbWxlcHVkaWZ3ZmJ4bXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0OTMwMjMsImV4cCI6MjA3MzA2OTAyM30.ST5z0h3iYmiTLcBrowYLj3_fnAKZ2lXDRBmxKNr9xxY',
+  );
 
   // TODO: Replace with your actual OneSignal App ID
-  // await NotificationService.instance.initialize('YOUR_ONESIGNAL_APP_ID');
+  await NotificationService.instance.initialize('49783777-40d1-4d08-8b7f-1a503d3aab64');
 
   // Initialize map service
   MapService.instance.initializeController();
@@ -39,7 +42,7 @@ class SafeHavenApp extends StatelessWidget {
       title: 'SafeHaven',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+          seedColor: const Color.fromARGB(255, 179, 179, 86),
           brightness: Brightness.light,
         ),
         useMaterial3: true,
@@ -50,7 +53,7 @@ class SafeHavenApp extends StatelessWidget {
       ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+          seedColor: const Color.fromARGB(255, 192, 209, 100),
           brightness: Brightness.dark,
         ),
         useMaterial3: true,
